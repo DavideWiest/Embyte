3389e74a61c714343e9dce28ffaaa0bce6f39399
Davide Wiest <davide.wiest2@gmail.com>
2023-09-17 17:47:55 +0200

    add domainname specification

--- Data/Storage/EmbyteStorage.cs
+++ Data/Storage/EmbyteStorage.cs
@@ -2,6 +2,7 @@
 
 public static class EmbyteStorage
 {
+    public static string domainName = "embyte.davidewiest.com";
     public static string DefaultUrl { get;  } = "https://github.com/DavideWiest";
     public static string DefaultUrl2 { get; } = "https://www.w3schools.com/html/default.asp";
 }
