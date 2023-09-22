4ddef33194d9297792e1e7f402b6789565ef4652
Davide Wiest <davide.wiest2@gmail.com>
2023-09-22 15:39:17 +0200

    comment out new feature for now

--- Data/Storage/EmbyteStorage.cs
+++ Data/Storage/EmbyteStorage.cs
@@ -8,6 +8,6 @@ public static class EmbyteStorage
     public static string relativeEmbedEndpoint = "/embed/";
     public static string documentationUrl = "https://github.com/DavideWiest/embyte/wiki";
     public static string DefaultUrl { get; } = "https://www.astronomy.com/picture-of-the-day/";
-    public static List<string> DefaultUrlList { get; } = new List<string> { "https://www.bbc.com/news" };
+    public static List<string> DefaultUrlList { get; } = new List<string> { "https://www.astronomy.com/picture-of-the-day/", "https://github.com", "https://embyte.davidewiest.com", "https://wikipedia.com", "https://nav.al" };
     public static string DefaultUrl2 { get; } = "https://github.com/DavideWiest";
 }
